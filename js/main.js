99160456607afec1987cf3071c8451a554e85fc7
zaratuustrat <timjimmik12@gmail.com>
2021-10-12 13:00:01 +0300

    create: section and fied hotel slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-const swiper = new Swiper('.swiper', {
-  keyboard: {
+var hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
     enabled: true,
     onlyInViewport: false,
+    
   },
-
-
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 });
+
+
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+   loop: true,
+   
+     // Navigation arrows
+     navigation: {
+       nextEl: '.reviews-slider__button--next',
+       prevEl: '.reviews-slider__button--prev',
+     },
+   });
