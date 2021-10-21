949d1863236c62eb17050098babfaf489ab321cd
zaratuustrat <timjimmik12@gmail.com>
2021-10-21 17:22:09 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ var hotelSlider = new Swiper('.hotel-slider', {
 });
 
 
-
 var reviewsSlider = new Swiper('.reviews-slider', {
    loop: true,
    
@@ -22,6 +21,14 @@ var reviewsSlider = new Swiper('.reviews-slider', {
        nextEl: '.reviews-slider__button--next',
        prevEl: '.reviews-slider__button--prev',
      },
+});
+
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+    document
+       .querySelector(".navbar-bottom")
+       .classList.toggle("navbar-bottom--visible");
    });
-
-
+   
