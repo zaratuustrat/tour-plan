f759a5728fe3e7815dc050ee959fcdaef75397f0
zaratuustrat <timjimmik12@gmail.com>
2021-10-27 14:44:37 +0300

    fixed: project

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-
+var hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
-  effect: "coverflow",
-});
-
-const reviewSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
   loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
 });
 
 
-
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  console.log('клик по кнопке меню');
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
-  document.querySelector('body').classList.toggle('hidden');
+var reviewsSlider = new Swiper('.reviews-slider', {
+   loop: true,
+   
+     // Navigation arrows
+     navigation: {
+       nextEl: '.reviews-slider__button--next',
+       prevEl: '.reviews-slider__button--prev',
+     },
 });
+
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+    document
+       .querySelector(".navbar-bottom")
+       .classList.toggle("navbar-bottom_visible");
+    document
+       .querySelector(".body")
+       .classList.toggle("body-scroll-off");   
+   });
+   
