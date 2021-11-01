a569a40d454e86c82658e0c9b5e4f8633433e7c9
zaratuustrat <timjimmik12@gmail.com>
2021-11-01 20:22:24 +0200

    add: input mask

--- js/main.js
+++ js/main.js
@@ -35,6 +35,18 @@ menuButton.addEventListener("click", function () {
        .querySelector(".body")
        .classList.toggle("body-scroll-off");   
    });
+
+//    var menuButton = document.querySelector(".booking__button");
+// menuButton.addEventListener("click", function () {
+//   console.log("Клик по кнопке меню");
+//     document
+//        .querySelector(".modal__overlay")
+//        .classList.toggle("modal__overlay--visible");
+//     document
+//        .querySelector(".body")
+//        .classList.toggle("body-scroll-off");   
+//    });
+   
    
 var modalButton = $("[data-togle=modal]");
 var closeModalButton = $(".modal__close");
@@ -54,5 +66,27 @@ function closeModal(event) {
   modalOverlay.removeClass("modal__overlay--visible");
   modalDialog.removeClass("modal__dialog--visible");
 }
+// Обработка форм
+$(".form").each(function() {
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Укажите ваше имя",
+        minlength: "Имя должно быть не короче двух букв",
+      },
+      email: {
+        required: "Укажите пожалуйста ваш Email",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "Телефон обязателен",
+      },
+    },
+  });
+});
 
+$(document).ready(function(){
+  $('.phone').mask('+7 (999) 999-99-99');
+ });
 });
